59354d601838dccc14abf2065441bbf22fa72cc2
João Sampaio <joao.sampaio.andrade@gmail.com>
2025-01-01 17:19:54 -0300

    initial: first commit with initial files

---
+++ core/configs.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+from sqlalchemy.orm.decl_api import DeclarativeMeta
+
+from typing import ClassVar
+
+
+class Settings(BaseSettings):
+    """
+    Configurações gerais usadas na aplicação
+    """
+    API_V1_STR: str = '/api/v1'
+    DB_URL: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/fastapi"
+    # DB_URL: str = "sqlite+aiosqlite:///faculdade.sqlite"
+    DBBaseModel: ClassVar[DeclarativeMeta] = declarative_base()
+    TEMPLATES: ClassVar[Jinja2Templates] = Jinja2Templates(directory='templates')
+    MEDIA: ClassVar[Path] = Path('media')
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
---
+++ models/models.py
@@ -0,0 +1,14 @@
+from typing import List, Optional
+from core.configs import settings
+from sqlalchemy import Column, Integer, String
+from pydantic import BaseModel
+
+class CursoModel(settings.DBBaseModel):
+    __tablename__ = "cursos"
+
+    id: int = Column(Integer, primary_key=True, autoincrement=True)
+    titulo: str = Column(String(100))
+    aulas: int = Column(Integer)
+    horas: int = Column(Integer)
+
+
