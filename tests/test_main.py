03b59933256f360c37a0902a140629accbadb501
JSampx <joao.sampaio.andrade@gmail.com>
2025-01-02 17:23:05 -0300

    refator: new code for testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,3 +9,10 @@ def test_get_cursos():
     response = client.get("/api/v1/cursos")
     assert response.status_code == 200
     # assert response.json() == {"msg": "Hello World"}
+
+def test_post_curso():
+
+    response = client.post("/api/v1/cursos", json={"titulo": "Hello World", "aulas": 12, "horas": 12},)
+
+    assert response.status_code == 201
+    assert response.json() == {"titulo": "Hello World", "aulas": 12, "horas": 12}
