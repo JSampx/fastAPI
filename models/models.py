eac6f0650ff0872fcc631154d6fab8035a643111
JSampx <joao.sampaio.andrade@gmail.com>
2025-01-07 20:57:29 -0300

    fix: changing relationship models

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 from core.configs import settings
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
 from pydantic import BaseModel
 
 class CursoModel(settings.DBBaseModel):
@@ -10,10 +11,12 @@ class CursoModel(settings.DBBaseModel):
     titulo: str = Column(String(100))
     aulas: int = Column(Integer)
     horas: int = Column(Integer)
+    autor = relationship('AutorModel', back_populates='cursos')
 
 
 class AutorModel(settings.DBBaseModel):
-    __tablename__ = "autor"
+    __tablename__ = "autores"
 
     id: int = Column(Integer, primary_key=True, autoincrement=True)
     nome: str = Column(String(100))
+    cursos = relationship("CursoModel", back_populates="autor", cascade="all, delete-orphan")
